70865c5a9593e8528bdb7f3b4b5608388e428990
jonrosario <jonrosariox@gmail.com>
2022-07-23 14:54:43 -0400

    backend setup complete

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const morgan = require('morgan')
+const cors = require('cors')
+require('./db/connection')
+const garageRoutes = require('./routes/garageRoutes')
+const userRoutes = require('./routes/userRoutes')
+
+app.use(core())
+app.use(morgan('dev'))
+app.use(express.json)
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/garage', garageRoutes)
+app.use('/user', userRoutes)
+
+app.get('/', (req, res) => {
+  res.json('My Whips')
+})
+
+
+app.listen(PORT, () => {
+  console.log('Im on port', PORT)
+})
