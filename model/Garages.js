8ded961d910e1e78b7777156f03a7cd658874771
jonrosario <jonrosariox@gmail.com>
2022-07-26 17:59:12 -0400

    fixed seed.js

--- model/Garages.js
+++ model/Garages.js
@@ -1,21 +1,19 @@
 const mongoose = require('mongoose')
 
 const garageSchema = new mongoose.Schema({
-  cars: [{
-    make: String,
-    model: String,
-    year: Number,
-    carName: String,
-    engine: String,
-    transmission: String,
-    HP: Number,
-    topSpeed: Number,
-    img: String,
-    zeroToSixty: String,
-    description: String,
-  }]
+  make: String,
+  model: String,
+  year: String,
+  carName: String,
+  engine: String,
+  transmission: String,
+  HP: String,
+  topSpeed: String,
+  img: String,
+  zeroToSixty: String,
+  description: String,
 })
 
 const Garage = mongoose.model('Garage', garageSchema)
 
-module.export = Garage
+module.exports = Garage
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const cors = require('cors')
 require('./db/connection')
 const garageRoutes = require('./routes/garageRoutes')
 const userRoutes = require('./routes/userRoutes')
+const methodOverride = require('method-override')
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json)
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/garage', garageRoutes)
 app.use('/user', userRoutes)
