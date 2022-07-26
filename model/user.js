13a457e72ad718e1a419c567655db1bb8180bca2
Corey Kim <98249558+Himecutatl@users.noreply.github.com>
2022-07-26 18:40:04 -0400

    Delete user.js
    deleting corey's initial commit

--- model/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema ({
-    name: String,
-    googleId: String,
-    garage: [{ref: 'Garage'}],
-})
