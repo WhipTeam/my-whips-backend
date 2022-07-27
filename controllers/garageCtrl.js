3a6adb8112aac38139f5d268cf05374be00d956e
aman-alem <alem.aman@gmail.com>
2022-07-27 13:01:14 -0500

    controller boilerplate coded

---
+++ controllers/garageCtrl.js
@@ -0,0 +1,19 @@
+const Garage = require("../models/Garage");
+
+const index = (req, res) => {};
+
+const create = (req, res) => {};
+
+const show = (req, res) => {};
+
+const update = (req, res) => {};
+
+const deleteWhip = (req, res) => {};
+
+module.exports = {
+  index,
+  create,
+  show,
+  update,
+  delete: deleteWhip,
+};
