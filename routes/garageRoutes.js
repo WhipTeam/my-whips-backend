4e581a430e42fbccaecf4eea03fceb06f6430419
aman-alem <alem.aman@gmail.com>
2022-07-27 14:32:54 -0500

    Added garage routes

--- routes/garageRoutes.js
+++ routes/garageRoutes.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
 const garageCtrl = require("../controllers/garageCtrl.js");
 
+// index
+router.get("/", garageCtrl.index);
+
+// create
+router.post("/", garageCtrl.create);
+
+// show
+router.get("/:id", garageCtrl.show);
+
+// update
+router.put("/:id", garageCtrl.update);
+
+// delete
+router.delete("/:id", garageCtrl.delete);
+
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const userCtrl = require("../controllers/userCtrl.js");
 
+// index
+router.get("/", userCtrl.index);
+
 module.exports = router;
