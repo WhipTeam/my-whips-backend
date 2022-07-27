7d0dfe5c1b8d8403c6404749ebce35acbd0838ce
aman-alem <alem.aman@gmail.com>
2022-07-27 13:06:16 -0500

    Coded models

---
+++ models/Garage.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const garageSchema = new mongoose.Schema({
+  cars: [
+    {
+      make: String,
+      model: String,
+      year: String,
+      carName: String,
+      engine: String,
+      transmission: String,
+      HP: String,
+      topSpeed: String,
+      img: String,
+      zeroToSixty: String,
+      description: String,
+    },
+  ],
+});
+
+const Garage = mongoose.model("Garage", garageSchema);
+
+module.exports = Garage;
