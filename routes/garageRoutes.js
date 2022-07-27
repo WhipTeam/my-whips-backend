2339e301393261521691b988e9f658f88c0a6923
aman-alem <alem.aman@gmail.com>
2022-07-27 12:54:47 -0500

    route file boilerplates coded and controller file names changed

---
+++ routes/garageRoutes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const garageCtrl = require("../controllers/garageCtrl.js");
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const userCtrl = require("../controllers/userCtrl.js");
+
+module.exports = router;
