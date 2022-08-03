14f2452ecb2cd1342411157a25c20c8ae5c6c1d9
aman-alem <alem.aman@gmail.com>
2022-08-03 16:51:52 -0500

    added check if user already exists in userController

--- controllers/userSignupCtrl.js
+++ controllers/userSignupCtrl.js
@@ -1,16 +1,21 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-const signup = (req, res) => {
-  bcrypt.hash(`${req.body.password}`, 8, (err, hash) => {
-    if (err) {
-      res.status(400).json(err);
-      return;
-    }
-    User.create({ name: req.body.name, password: hash }).then((user) => {
-      res.json(user);
+const signup = async (req, res) => {
+  let user = await User.findOne({ name: req.body.name });
+  if (user) {
+    res.json(`this user already exists`);
+  } else {
+    bcrypt.hash(`${req.body.password}`, 8, (err, hash) => {
+      if (err) {
+        res.status(400).json(err);
+        return;
+      }
+      User.create({ name: req.body.name, password: hash }).then((user) => {
+        res.json(user);
+      });
     });
-  });
+  }
 };
 
 module.exports = {
